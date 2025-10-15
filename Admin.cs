436f860393e8003b1c304886eaf4fae69e5c6970
linahalle <hallergren.lina@gmail.com>
2025-10-15 11:39:17 +0200

    added the code for adding a new location, in admin menu

--- Admin.cs
+++ Admin.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace HCS;
 
 public class Admin : IUser
@@ -99,6 +101,57 @@ public class Admin : IUser
                     break;
 
                 case "3":
+                    //add locations;
+                    try { Console.Clear(); } catch { }
+                    Console.WriteLine("Write in which region: blekinge, halland, skåne, kronoberg?");
+                    string choosenRegion = Console.ReadLine();
+                    AllRegions selectedRegion = AllRegions.none;
+                    switch (choosenRegion)
+                    {
+                        case "blekinge":
+                            selectedRegion = AllRegions.Blekinge;
+                            Console.WriteLine("Write the name of the new reception");
+                            string newReceptionBlekinge = Console.ReadLine();
+
+                            Location newlocationBlekinge = new Location(newReceptionBlekinge, AllRegions.Blekinge);
+                            Console.WriteLine($"New reception: {newReceptionBlekinge} was added in Region {AllRegions.Blekinge}");
+                            Console.WriteLine("Press ENTER to go back to menu");
+                            Console.ReadLine();
+                            break;
+
+                        case "halland":
+                            selectedRegion = AllRegions.Halland;
+                            Console.WriteLine("Write the name of the new reception");
+                            string newReceptionHalland = Console.ReadLine();
+
+                            Location newlocationHalland = new Location(newReceptionHalland, AllRegions.Halland);
+                            Console.WriteLine($"New reception: {newReceptionHalland} was added in Region {AllRegions.Halland}");
+                            Console.WriteLine("Press ENTER to go back to menu");
+                            Console.ReadLine();
+                            break;
+
+                        case "skåne":
+                            selectedRegion = AllRegions.Skåne;
+
+                            Console.WriteLine("Write the name of the new reception");
+                            string newReceptionSkåne = Console.ReadLine();
+                            Location newlocationSkåne = new Location(newReceptionSkåne, AllRegions.Skåne);
+                            Console.WriteLine($"New reception: {newReceptionSkåne} was added in Region {AllRegions.Skåne}");
+                            Console.WriteLine("Press ENTER to go back to menu");
+                            Console.ReadLine();
+                            break;
+
+                        case "kronoberg":
+                            selectedRegion = AllRegions.Kronoberg;
+                            Console.WriteLine("Write the name of the new reception");
+                            string newReceptionKronoberg = Console.ReadLine();
+
+                            Location newlocationKronoberg = new Location(newReceptionKronoberg, AllRegions.Skåne);
+                            Console.WriteLine($"New reception: {newReceptionKronoberg} was added in Region {AllRegions.Kronoberg}");
+                            Console.WriteLine("Press ENTER to go back to menu");
+                            Console.ReadLine();
+                            break;
+                    }
                     break;
 
                 case "4":
